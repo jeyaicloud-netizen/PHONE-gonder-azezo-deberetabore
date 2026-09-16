6ee2fc076542b315f458c75f6c50cdc1e3f1aa23
jeyaicloud-netizen <jeyaicloud@gmail.com>
2026-09-16 18:04:00 +0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect, useRef, useMemo } from 'react';
 import { Contact, CallLog, SimConfig, ActiveCallState } from './types';
-import { INITIAL_CONTACTS, INITIAL_CALL_LOGS, INITIAL_SIMS } from './data/initialData';
+import { initialContacts, initialCallLogs, defaultSettings } from './initialData';
+import { playKeypadTone, triggerHaptic, playCallEndTone } from './audio';
 import StatusBar from './components/StatusBar';
 import SearchHeader from './components/SearchHeader';
 import CallHistoryList from './components/CallHistoryList';
